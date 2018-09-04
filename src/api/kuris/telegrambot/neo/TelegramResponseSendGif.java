eb659111513c9579df4fa01dd09d5c404f90b1d0
Kuris <kurisotaku@gmail.com>
2018-09-03 21:16:11 -0300

    Corrigido mapeamento do construtor da classe TelegramResponseSendGif. Alguns elementos do objeto json retornado são ignorados.

--- src/api/kuris/telegrambot/neo/TelegramResponseSendGif.java
+++ src/api/kuris/telegrambot/neo/TelegramResponseSendGif.java
@@ -6,8 +6,6 @@
 package api.kuris.telegrambot.neo;
 
 import java.util.Date;
-import org.json.JSONArray;
-import org.json.JSONException;
 import org.json.JSONObject;
 
 /**
@@ -17,12 +15,42 @@ import org.json.JSONObject;
 public class TelegramResponseSendGif {
 
     protected String token;
-    protected boolean exist_messages;
-    protected int update_id;
-    protected TelegramUpdateMessage message;
-    protected TelegramUpdateEdited_message edited_message;
-    protected TelegramUpdateInline_query inline_query;
-    protected TelegramUpdateCallback_query callback_query;
+    protected String file_id;
+    protected int file_size;
+    protected int message_id;
+    protected String error_description;
+    protected int error_code;
+    protected boolean ok;
+
+    protected TelegramUpdateDocument document;
+    protected TelegramUpdateChat chat;
+
+    public TelegramResponseSendGif(String json, String token) {
+        JSONObject jsonObj = new JSONObject(json);
+        try {
+            JSONObject result = jsonObj.getJSONObject("result");
+            this.token = token;
+            this.document = new TelegramUpdateDocument(result);
+            this.file_id = document.getFile_id();
+            this.file_size = document.getFile_size();
+            this.chat = new TelegramUpdateChat(result);
+            this.message_id = result.getInt("message_id");
+            this.error_code = 0;
+            this.error_description = "#null";
+            this.ok = true;
+        } catch (Exception pegaErro1) {
+            JSONObject result = jsonObj.getJSONObject("result");
+            this.token = token;
+            this.document = null;
+            this.file_id = null;
+            this.file_size = 0;
+            this.chat = null;
+            this.message_id = 0;
+            this.ok = false;
+            this.error_code = new JSONObject(json).getInt("error_code");
+            this.error_description = new JSONObject(json).getString("description");
+        }
+    }
 
     public String Token() {
         return token;
@@ -32,178 +60,4 @@ public class TelegramResponseSendGif {
         this.token = token;
     }
 
-    public boolean isExist_messages() {
-        return exist_messages;
-    }
-
-    public void Exist_messages(boolean exist_messages) {
-        this.exist_messages = exist_messages;
-    }
-
-    public TelegramUpdateCallback_query Callback_query() {
-        return callback_query;
-    }
-
-    public void Callback_query(TelegramUpdateCallback_query callback_query) {
-        this.callback_query = callback_query;
-    }
-
-    public int Update_id() {
-        return update_id;
-    }
-
-    public TelegramUpdateMessage Message() {
-        return message;
-    }
-
-    public TelegramUpdateEdited_message Edited_message() {
-        return edited_message;
-    }
-
-    public TelegramUpdateInline_query Inline_query() {
-        return inline_query;
-    }
-
-    // s DE OBJETOS INFERIORES
-    public long userID() {
-        return message.from.id_user;
-    }
-
-    public long chatID() {
-        if (callback_query != null) {
-            return callback_query.from().id_user;
-        } else {
-            return message.chat.id_chat;
-        }
-    }
-
-    public void text(String text) {
-        this.message.text = text;
-    }
-    
-
-    public String text() {
-        if (message == null) {
-            return callback_query.Data();
-        } else {
-            return message.text;
-        }
-    }
-
-    public int messageId() {
-        return message.message_id;
-    }
-
-    public String entiteType() {
-        if (message.entities == null) {
-            return null;
-        } else {
-            return message.entities.type;
-        }
-    }
-
-    public Date date() {
-        return message.date;
-    }
-
-    public TelegramUpdateFrom userInfo() {
-        return message.from;
-    }
-
-    /*    public JSONObject getMessage() {
-        return message;
-    }
-     */
-    
-    public TelegramResponseSendGif(String json, String token) throws JSONException {
-        System.out.println(json);
-        /*
-{
-  "ok": true,
-  "result": {
-    "message_id": 2722,
-    "from": {
-      "id": 304076906,
-      "is_bot": true,
-      "first_name": "Newaii 😴",
-      "username": "New_AI_bot"
-    },
-    "chat": {
-      "id": 52022517,
-      "first_name": "🇰 🇺 🇷 🇮 🇸",
-      "last_name": "クリス",
-      "username": "Kuris",
-      "type": "private"
-    },
-    "date": 1535826715,
-    "animation": {
-      "file_name": "giphy.mp4",
-      "mime_type": "video/mp4",
-      "duration": 3,
-      "width": 480,
-      "height": 272,
-      "thumb": {
-        "file_id": "AAQEABPqWmEZAATK5Ur2Q8m0tw0fAQABAg",
-        "file_size": 2273,
-        "width": 90,
-        "height": 51
-      },
-      "file_id": "CgADBAADj4sAAugaZAfrx4Ee7eHbvgI",
-      "file_size": 238798
-    },
-    "document": {
-      "file_name": "giphy.mp4",
-      "mime_type": "video/mp4",
-      "thumb": {
-        "file_id": "AAQEABPqWmEZAATK5Ur2Q8m0tw0fAQABAg",
-        "file_size": 2273,
-        "width": 90,
-        "height": 51
-      },
-      "file_id": "CgADBAADj4sAAugaZAfrx4Ee7eHbvgI",
-      "file_size": 238798
-    }
-  }
-}
-        */
-        JSONObject jsonObj = new JSONObject(json);
-        try {
-            JSONArray results = jsonObj.getJSONArray("result");
-            JSONObject updates = results.getJSONObject(0);
-            try {
-                this.update_id = updates.getInt("update_id");
-            } catch (Exception tr_update) {
-                this.update_id = 0;
-            }
-            try {
-                try {
-                    this.message = new TelegramUpdateMessage(updates.getJSONObject("message"), token);
-                    this.edited_message = null;
-                } catch (Exception tr4) {
-                    this.message = null;
-                    this.edited_message = new TelegramUpdateEdited_message(updates.getJSONObject("edited_message"), token);
-                }
-            } catch (Exception tr6) {
-                this.message = null;
-                this.edited_message = null;
-            }
-            try {
-                this.callback_query = new TelegramUpdateCallback_query(updates.getJSONObject("callback_query"), token);
-            } catch (Exception tr9) {
-                this.callback_query = null;
-            }
-            try {
-                this.inline_query = new TelegramUpdateInline_query(updates);
-            } catch (Exception tr5) {
-                this.inline_query = null;
-            }
-            this.exist_messages = true;
-        } catch (Exception tr8) {
-            this.exist_messages = false;
-        }
-
-    }
-
-
-    
 }
--- src/api/kuris/telegrambot/neo/TelegramUpdateDocument.java
+++ src/api/kuris/telegrambot/neo/TelegramUpdateDocument.java
@@ -16,11 +16,17 @@ public class TelegramUpdateDocument {
 
     protected String file_name;
     protected String file_id; // se repete
+    protected int file_size;
+
+    public int getFile_size() {
+        return file_size;
+    }
 
     public TelegramUpdateDocument(JSONObject message) throws JSONException {
         JSONObject document = message.getJSONObject("document");
         this.file_name = document.getString("file_name");
-                this.file_id = document.getString("file_id");
+        this.file_id = document.getString("file_id");
+        this.file_size = document.getInt("file_size");
     }
 
     public String getFile_name() {
